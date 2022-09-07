b51786df329c5781b1ce759da995f2c08108d5c4
Marko Rauhamaa <marko@pacujo.net>
2022-09-07 21:01:24 +0300

    Turn DuplexBody into a ByteStreamBody

--- src/misc/duplex.rs
+++ src/misc/duplex.rs
@@ -1,63 +1,93 @@
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 use std::io::Result;
 
-use crate::{Disk, WeakDisk, Link, UID, Action, Registration, Fd};
-use crate::{Downgradable, DECLARE_LINKS};
-use crate::stream::{ByteStream, ByteStreamPair, ByteStreamPairBody};
-use crate::stream::{DebuggableByteStreamPairBody, switch, file, dry, empty};
+use crate::{Disk, Link, UID, Action, Registration, Fd};
+use crate::{Downgradable, DECLARE_LINKS, IMPL_STREAM};
+use crate::stream::{ByteStream, ByteStreamBody, DebuggableByteStreamBody};
+use crate::stream::{ByteStreamPair, ByteStreamPairBody};
+use crate::stream::{DebuggableByteStreamPairBody};
+use crate::stream::{base, switch, file, dry};
 use crate::misc::Linger;
 use r3::{TRACE, Traceable};
 
 #[derive(Debug)]
 pub struct DuplexBody {
-    weak_disk: WeakDisk,
-    uid: UID,
-    ingress: Option<switch::Stream>,
+    base: base::StreamBody,
+    weak_self: Weak<RefCell<DuplexBody>>,
+    ingress: ByteStream,
     egress: Option<Linger>,
     eswitch: Option<switch::Stream>,
     registration: Option<Registration>,
 }
 
 impl DuplexBody {
-    fn shut_down_ingress(&mut self) {
-        if let Some(disk) = self.weak_disk.upgrade() {
-            if let Some(switch) = self.ingress.take() {
-                switch.switch(empty::Stream::new(&disk).as_bytestream());
-            }
-        }
-    }
-
-    fn shut_down_egress(&mut self) {
-        if let Some(linger) = self.egress.take() {
-            linger.abort();
-        }
-        self.eswitch.take();
+    fn read_nontrivial(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.ingress.read(buf)
     }
 
     fn notify(&self) {
-        if let Some(inswitch) = &self.ingress {
-            inswitch.invoke_callback();
-        }
+        self.base.invoke_callback();
         if let Some(egress) = &self.egress {
             egress.prod();
         }
     }
 } // impl DuplexBody
 
+impl ByteStreamBody for DuplexBody {
+    fn register_callback(&mut self, callback: Action) {
+        TRACE!(ATEN_DUPLEX_REGISTER_CALLBACK {
+            STREAM: self, ACTION: &callback
+        });
+        self.base.register_callback(callback);
+    }
+
+    fn unregister_callback(&mut self) {
+        TRACE!(ATEN_DUPLEX_UNREGISTER_CALLBACK { STREAM: self });
+        self.base.unregister_callback();
+    }
+
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        if let Ok(_) = self.base.read(buf) {
+            TRACE!(ATEN_DUPLEX_READ_TRIVIAL { STREAM: self, WANT: buf.len() });
+            return Ok(0);
+        }
+        match self.read_nontrivial(buf) {
+            Ok(count) => {
+                TRACE!(ATEN_DUPLEX_READ {
+                    STREAM: self, WANT: buf.len(), GOT: count
+                });
+                TRACE!(ATEN_DUPLEX_READ_DUMP {
+                    STREAM: self, DATA: r3::octets(&buf[..count])
+                });
+                Ok(count)
+            }
+            Err(err) => {
+                TRACE!(ATEN_DUPLEX_READ_FAIL {
+                    STREAM: self, WANT: buf.len(), ERR: r3::errsym(&err)
+                });
+                Err(err)
+            }
+        }
+    }
+}
+
 impl std::fmt::Display for DuplexBody {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.uid)
+        write!(f, "{}", self.base.get_uid())
     }
 } // impl std::fmt::Display for StreamBody
 
+impl DebuggableByteStreamBody for DuplexBody {}
+
 impl ByteStreamPairBody for DuplexBody {
     fn get_ingress(&self) -> Option<ByteStream> {
-        self.ingress.as_ref().map(|s| s.as_bytestream())
+        self.weak_self.upgrade().map(|s| ByteStream::new(self.base.get_uid(), s))
     }
 
     fn set_egress(&mut self, egress: ByteStream) {
         if let Some(eswitch) = &self.eswitch {
+            TRACE!(ATEN_DUPLEX_SET_EGRESS { DUPLEX: self, EGRESS: egress });
             eswitch.switch(egress);
         }
     }
@@ -65,27 +95,39 @@ impl ByteStreamPairBody for DuplexBody {
 
 impl DebuggableByteStreamPairBody for DuplexBody {}
 
+impl Drop for DuplexBody {
+    fn drop(&mut self) {
+        TRACE!(ATEN_DUPLEX_DROP { DUPLEX: self });
+    }
+}
+
 DECLARE_LINKS!(Duplex, WeakDuplex, DuplexBody, ATEN_DUPLEX_UPPED_MISS, DUPLEX);
 
 impl Duplex {
+    IMPL_STREAM!();
+
     pub fn new(disk: &Disk, fd: &Fd) -> Result<Duplex> {
         let uid = UID::new();
         let eswitch = switch::Stream::new(
             &disk, dry::Stream::new(&disk).as_bytestream());
-        let body = DuplexBody {
-            weak_disk: disk.downgrade(),
-            uid: uid,
-            ingress: Some(switch::Stream::new(
-                &disk, file::Stream::new(
-                    &disk, &fd, true).unwrap().as_bytestream())),
-            egress: Some(Linger::new(
-                &disk, eswitch.as_bytestream(), &fd, true).unwrap()),
-            eswitch: Some(eswitch),
-            registration: None,
-        };
+        let ingress = file::Stream::new(
+            &disk, &fd, true).unwrap().as_bytestream();
+        let egress = Linger::new(
+            &disk, eswitch.as_bytestream(), &fd, true).unwrap();
+        let body = Rc::new_cyclic(
+            |weak_self| RefCell::new(
+                DuplexBody {
+                    base: base::StreamBody::new(disk.downgrade(), uid),
+                    weak_self: weak_self.clone(),
+                    ingress: ingress.clone(),
+                    egress: Some(egress.clone()),
+                    eswitch: Some(eswitch),
+                    registration: None,
+                }
+            ));
         let duplex = Duplex(Link {
             uid: uid,
-            body: Rc::new(RefCell::new(body)),
+            body: body,
         });
         let weak_duplex = duplex.downgrade();
         let notify = Action::new(move || {
@@ -103,7 +145,10 @@ impl Duplex {
                 return Err(err);
             }
         }
-        TRACE!(ATEN_DUPLEX_CREATE { DISK: disk, DUPLEX: uid, FD: fd });
+        TRACE!(ATEN_DUPLEX_CREATE {
+            DISK: disk, DUPLEX: uid, FD: fd, INGRESS: ingress,
+            EGRESS_LINGER: egress,
+        });
         Ok(duplex)
     }
 
